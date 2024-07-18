af1a7ad44aed3bacc102f941b2caecf88080ed12
Jungman Bae <jmbae@liv.kr>
2024-07-18 13:29:58 +0900

    엔트리 추가 기능 수정, 테스트 코드 수정 (통과)

---
+++ EntryApp/EntryApp/Views/AddEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  AddEntryView.swift
+//  EntryApp
+//
+//  Created by Jungman Bae on 7/18/24.
+//
+
+import SwiftUI
+
+struct AddEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEntryView()
+}
