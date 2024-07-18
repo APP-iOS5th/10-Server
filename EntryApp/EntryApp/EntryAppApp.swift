4515c1e5eb74c3c7150f9c60e9617fe6e19af12b
jmbae <jmbae@codegrove.co.kr>
2024-07-18 09:27:41 +0900

    Initialize EntryApp

---
+++ EntryApp/EntryApp/EntryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntryAppApp.swift
+//  EntryApp
+//
+//  Created by Jungman Bae on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct EntryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
